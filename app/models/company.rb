2cc90233510340d89cd8e82a4a7203b80199ec35
unknown <Administrator@celab-1-17.comp1-domain.spit.ac.in>
2012-09-16 14:57:48 +0530

    validatin qty working

--- app/models/company.rb
+++ app/models/company.rb
@@ -1,2 +1,8 @@
 class Company < ActiveRecord::Base
+  validate :symbol_len_2_or_3
+  def symbol_len_2_or_3
+    if symbol.length<2 || symbol.length>3
+      errors.add :symbol,"Should be 2 or 3 characters"
+    end
+end
 end
